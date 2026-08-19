93ff5d176770d4baa64cfc28e415f2e20798b19d
Duncan Lockie <Duncan.Lockie@gov.scot>
2026-08-19 14:08:22 +0100

    fix typo

--- R/condition.R
+++ R/condition.R
@@ -8,9 +8,15 @@ create_unclipped_basemap <- function(lcs_88_std,
                                      lca_std,
                                      lcs_88_condition_lookup){
   
+  # datasets
+  
   lcs_88 <- terra::rast(lcs_88_std)
   
+  lca <- terra::rast(lca_std)
+  
   lookup <- lcs_88_condition_lookup
+  
+  # labels 
   
   labels <- terra::levels(lcs_88)[[1]]
 
@@ -19,21 +25,39 @@ create_unclipped_basemap <- function(lcs_88_std,
     lookup,
     by = "DOMTEXT"
   ) |> 
+    dplyr::bind_rows(
+      data.frame(value = 200, # add value to facilitate uplands correction
+                 DOMTEXT = "LCA Uplands Correction",
+                 Condition = "LCA Uplands Correction")
+    ) |> 
     dplyr::arrange(Condition) |> 
     dplyr::group_by(Condition) |>
     dplyr::mutate(
       condition_value = dplyr::cur_group_id()
     ) |>
-    dplyr::ungroup()
+    dplyr::ungroup() 
+    
 
+  # classify 
+  
   lcs_88 <- terra::classify(
     lcs_88,
     labels |> dplyr::select(value, condition_value)
   )
   
+  # uplands correction
+
+  uplands_correction_indices <- lcs_88$DOMTEXT == 4 & # ! Note hard-coded value!
+    lca$SqMid == "Uplands"
+
+  lcs_88[uplands_correction_indices] <- 8 # !Note hard-coded value!
+  
+  # update levels
+  
   levels(lcs_88) <- labels |> dplyr::select(condition_value, Condition) |> 
     dplyr::distinct()
   
+  # save output
   output_path <- fs::path("data", "processed", "basemap_unclipped.tif")
   
   terra::writeRaster(
