ad2beba2f9a1e37210b3c6979bb1b75bb887e8c0
Duncan Lockie <Duncan.Lockie@gov.scot>
2026-08-26 16:01:36 +0100

    remove land area calculation and change nas to mapping offset

--- R/condition.R
+++ R/condition.R
@@ -72,9 +72,18 @@ create_unclipped_basemap <- function(lcs_88_std,
     lcs_88$DOMTEXT == extensive_grassland_value &
     lca$SqMid == uplands_code
   
+  mapping_offset_value <- labels |>
+    dplyr::filter(condition == "mapping offset") |>
+    dplyr::pull(condition_value) |> 
+    dplyr::first()
+  
   cli::cli_alert_info("Unclipped basemap: apply uplands correction - {Sys.time()}")
   
   lcs_88[uplands_correction_indices] <- uplands_correction_value
+  
+  cli::cli_alert_info("Unclipped basemap: NA to mapping offset - {Sys.time()}")
+  
+  lcs_88 <- terra::ifel(is.na(lcs_88), mapping_offset_value, lcs_88)
 
   cli::cli_alert_info("Unclipped basemap: update levels and crs - {Sys.time()}")
   
@@ -165,9 +174,6 @@ summarise_condition_crosstab <- function(extent_path, boundary_path, condition_p
            boundary_class = boundary_class_name,
            boundary_name = as.character(boundary_name),
            .keep = 'unused') |> 
-    group_by(boundary_name) |>
-    mutate(land_area_ha = sum(area_ha, na.rm = TRUE)) |>
-    ungroup() |>
     pivot_wider(names_from = peat_depth_class,
                 names_prefix = "class_",
                 values_from = area_ha,
@@ -207,8 +213,7 @@ summarise_condition_crosstab <- function(extent_path, boundary_path, condition_p
       area_ha,
       total_pd_50,
       total_pd_40,
-      total_pd_30,
-      land_area_ha
+      total_pd_30
     )
 
   output
